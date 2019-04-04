b69c703be9f1c5e2a0a956bb60a41afff5fae6a8
Aymara <aysanchez@tesselar.mx>
2019-04-04 11:37:04 -0600

    carrusel de mas vendidos creado con slick, se le aplicaron los estilos

--- js/bestseller.js
+++ js/bestseller.js
@@ -1,38 +1,35 @@
-var data;
+$(document).ready(function () {
 
-$.ajax({
-        url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search/?fq=productClusterIds:140?fq=specificationFilter_79:1"
-    })
-    .done(function (responseData) {
-        data = responseData;
-        renderData();
-    });
+    var data;
 
-function renderData() {
-    console.log(data);
-    console.log(data[0].items[0].images[0].imageUrl);
-    console.log(data[0].items);
-    console.log(data[0].items[0].images);
-    console.log(data[0].items[0].sellers[0].commertialOffer.Price);
-    console.log(data[0].link);
+    $.ajax({
+            url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search/?fq=productClusterIds:140?fq=specificationFilter_79:1"
+        })
+        .done(function (responseData) {
+            data = responseData;
+            renderData();
+        });
 
-    var container = document.getElementById('cards-productos-mas-vendidos');
-    var html = "<div>";
+    function renderData() {
 
-    for (let i = 0; i < data.length; i++) {
-        const element = data[i];
+        console.log(data);
 
-        var imgDomElmt = '<img class="productImg" src="' + element.items[0].images[0].imageUrl + '"/>';
-        var lamName = '<p class="nameLamina">' + element.productName + '</p>';
-        var lamActPrice = '<p class="actualprice">$' + element.items[0].sellers[0].commertialOffer.Price + ' </p>';
-        var lamButtom = '<a href="' + element.link + '" class="productButton" style="cursor:pointer">VER MÁS</a>';
+        var container = document.getElementById('mas-vendidos-carrusel');
+        var html;
 
-        html += '<div class="laminas">' +
-            imgDomElmt + lamName + lamActPrice + lamButtom;
-        html += '</div>';
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+
+            var imgDomElmt = '<img class="productImg" src="' + element.items[0].images[0].imageUrl + '"/>';
+            var lamName = '<p class="nameLamina">' + element.productName + '</p>';
+            var lamActPrice = '<p class="actualPrice">$' + element.items[0].sellers[0].commertialOffer.Price + ' MXN </p>';
+            var lamButtom = '<a href="' + element.link + '" class="productButton" style="cursor:pointer">AÑADIR A LA BOLSA</a>';
+
+            html += '<div class="laminas">' +
+                imgDomElmt + lamName + lamActPrice + lamButtom;
+            html += '</div>';
+        }
+
+        container.innerHTML = html;
     }
-
-    html += "</div>";
-
-    container.innerHTML = html;
-}
+});
