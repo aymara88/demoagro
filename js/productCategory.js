492532c33cdbba2fd1d47298ea2356db9b5fa58b
Aymara <aysanchez@tesselar.mx>
2019-04-11 17:09:40 -0500

    trabajo en la pagina de categorias que muestra los productos, realizado el select con el metodo onChange para cambiar el orden de los productos

--- js/productCategory.js
+++ js/productCategory.js
@@ -1,6 +1,22 @@
-function productCategory(category, categoryLink) {
-    console.log(category);
-    console.log(categoryLink);
+function lamProductIdCategory(data, category) {
+    let container = document.getElementById('productos-categoria');
+    let html;
+    for (var i = 0; i < data.length; i++) {
+        const element = data[i];
+        if (element.categoryId == category) {
+            html +=
+                `<div class="laminas">
+                <a href="${element.link}" style="cursor:pointer"><img class="productImg" src="${element.items[0].images[0].imageUrl}"/></a>
+                <a href="${element.link}" style="cursor:pointer"><p class="nameLamina">${element.productName}</p></a>
+                <a href="${element.link}" style="cursor:pointer"><p class="actualPrice">$ ${element.items[0].sellers[0].commertialOffer.Price} MXN </p></a>
+                <a href="${element.items[0].sellers[0].addToCartLink}" class="productButton" style="cursor:pointer">AÑADIR A LA BOLSA</a>
+            </div>`;
+        }
+    }
+    container.innerHTML = html;
+}
+
+function productCategoryASC(category) {
     var data;
 
     $.ajax({
@@ -9,36 +25,67 @@ function productCategory(category, categoryLink) {
         .done(function (responseData) {
             data = responseData;
             console.log(data);
-            lamProductIdCategory();
+            lamProductIdCategory(data, category);
         });
-
-    function lamProductIdCategory() {
-        /*let container = document.getElementById('productos-categoria');
-        console.log(container);*/
-        let html;
-        for (var i = 0; i < data.length; i++) {
-            const element = data[i];
-            if (element.categoryId == category) {
-                var imgDomElmt = '<a href="' + element.link + '" style="cursor:pointer"><img class="productImg" src="' + element.items[0].images[0].imageUrl + '"/></a>';
-                var lamName = '<a href="' + element.link + '" style="cursor:pointer"><p class="nameLamina">' + element.productName + '</p></a>';
-                var lamActPrice = '<a href="' + element.link + '" style="cursor:pointer"><p class="actualPrice">$' + element.items[0].sellers[0].commertialOffer.Price + ' MXN </p></a>';
-                var lamButtom = '<a href="' + element.items[0].sellers[0].addToCartLink + '" class="productButton" style="cursor:pointer">AÑADIR A LA BOLSA</a>';
-
-                html += '<div class="laminas">' +
-                    imgDomElmt + lamName + lamActPrice + lamButtom;
-                html += '</div>';
-                console.log(html);
-            }
-        }
-
-        return html;
-        /* var myWindow = window.open('https://demoagro.myvtex.com/agro-categorias/' + categoryLink + '', "_self");
-        myWindow.document.write(html); */
-
-        /*container.innerHTML = html;+/
-
-    }
-
 }
 
+function productCategoryDESC(category) {
+    var data;
 
+    $.ajax({
+            url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameDESC&fq=C:/23/"
+        })
+        .done(function (responseData) {
+            data = responseData;
+            console.log(data);
+            lamProductIdCategory(data, category);
+        });
+}
+
+function productCategoryBestRate(category) {
+    var data;
+
+    $.ajax({
+            url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByReviewRateDESC&fq=C:/23/"
+        })
+        .done(function (responseData) {
+            data = responseData;
+            console.log(data);
+            lamProductIdCategory(data, category);
+        });
+}
+
+function productCategoryReleaseDate(category) {
+    var data;
+
+    $.ajax({
+            url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByReleaseDateDESC&fq=C:/23/"
+        })
+        .done(function (responseData) {
+            data = responseData;
+            console.log(data);
+            lamProductIdCategory(data, category);
+        });
+}
+
+function getParameterByName(catId) {
+    catId = catId.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + catId + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function myOrderOfProducts(catId) {
+    let typeOfOrder = document.getElementById("selectOrder").value;
+    console.log(catId);
+
+    if (typeOfOrder == 'asc') {
+        productCategoryASC(catId);
+    } else if (typeOfOrder == 'desc') {
+        productCategoryDESC(catId);
+    } else if (typeOfOrder == 'bestrate') {
+        productCategoryBestRate(catId);
+    } else if (typeOfOrder == 'releasedate') {
+        productCategoryReleaseDate(catId);
+    }
+}
