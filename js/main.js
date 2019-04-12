9d270a1ae4d697a6dc09629673f67d0b08566e73
Aymara <aysanchez@tesselar.mx>
2019-04-12 16:51:44 -0500

    finalizada la carga de productos en la pagina de categorias segun id de categoria y tmb segun el tipo de ordenamiento en sus opciones del select

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 }))*/
 
 ($(document).ready(function () {
-  $("#2banner1 .box-banner").prepend("<a href='https://demoagro.myvtex.com/agro-categorias/vegetales-organicos'><h2 class='texto-2banner1 text-left'>SALUDABLE Y DELICIOSO</h2></a>");
-  $("#2banner1 .box-banner").prepend("<a id='boton-2banner1' class='btn btn-default btn-gris boton-banner' href='https://demoagro.myvtex.com/agro-categorias/vegetales-organicos' role='button'>VER MÁS</a>");
+  $("#2banner1 .box-banner").prepend("<a href='/categoriasAgro?catId=28'><h2 class='texto-2banner1 text-left'>SALUDABLE Y DELICIOSO</h2></a>");
+  $("#2banner1 .box-banner").prepend("<a id='boton-2banner1' class='btn btn-default btn-gris boton-banner' href='/categoriasAgro?catId=28' role='button'>VER MÁS</a>");
 
-  $("#2banner2 .box-banner").prepend("<a href='https://demoagro.myvtex.com/agro-categorias/frutas-organicas'><h2 class='texto-2banner2 text-right'>MOMENTOS DULCES</h2></a>");
-  $("#2banner2 .box-banner").prepend("<a id='boton-2banner2' class='btn btn-default btn-gris boton-banner' href='https://demoagro.myvtex.com/agro-categorias/frutas-organicas' role='button'>VER MÁS</a>");
+  $("#2banner2 .box-banner").prepend("<a href='/categoriasAgro?catId=27'><h2 class='texto-2banner2 text-right'>MOMENTOS DULCES</h2></a>");
+  $("#2banner2 .box-banner").prepend("<a id='boton-2banner2' class='btn btn-default btn-gris boton-banner' href='/categoriasAgro?catId=27' role='button'>VER MÁS</a>");
 
-  $("#2banner3 .box-banner").prepend("<a href='https://demoagro.myvtex.com/agro-categorias/especias-de-tu-cocina'><h2 class='texto-2banner3 text-right'>EL SABOR DE LO ÚNICO</h2></a>");
-  $("#2banner3 .box-banner").prepend("<a id='boton-2banner3' class='btn btn-default btn-gris boton-banner' href='https://demoagro.myvtex.com/agro-categorias/especias-de-tu-cocina' role='button'>VER MÁS</a>");
+  $("#2banner3 .box-banner").prepend("<a href='/categoriasAgro?catId=29'><h2 class='texto-2banner3 text-right'>EL SABOR DE LO ÚNICO</h2></a>");
+  $("#2banner3 .box-banner").prepend("<a id='boton-2banner3' class='btn btn-default btn-gris boton-banner' href='/categoriasAgro?catId=29' role='button'>VER MÁS</a>");
 }))
--- js/productCategory.js
+++ js/productCategory.js
@@ -1,9 +1,13 @@
+/* funcion para la creacion de las laminas de productos*/
 function lamProductIdCategory(data, category) {
     let container = document.getElementById('productos-categoria');
-    let html;
+    let cantProd = document.getElementById('cantProd');
+    let count = 0;
+    let html = "<div>";
     for (var i = 0; i < data.length; i++) {
         const element = data[i];
         if (element.categoryId == category) {
+            count++;
             html +=
                 `<div class="laminas">
                 <a href="${element.link}" style="cursor:pointer"><img class="productImg" src="${element.items[0].images[0].imageUrl}"/></a>
@@ -13,9 +17,43 @@ function lamProductIdCategory(data, category) {
             </div>`;
         }
     }
+    html += "</div>";
     container.innerHTML = html;
+    cantProd.innerHTML = count;
 }
 
+/* funcion para la creacion de las laminas de productos cuando estan ordenadas por mas o mejor vendidos*/
+function lamProductIdCategoryBestSeller(data, category) {
+    let container = document.getElementById('productos-categoria');
+    let cantProd = document.getElementById('cantProd');
+    let html = "<div>";
+    let arrProd = [];
+    for (var i = 0; i < data.length; i++) {
+        const element = data[i];
+        if (element.categoryId == category) {
+            if (element['Más Vendido'] == '1') {
+                arrProd.unshift(element);
+            } else {
+                arrProd.push(element);
+            }
+        }
+    }
+    for (var i = 0; i < arrProd.length; i++) {
+        const element = arrProd[i];
+        html +=
+            `<div class="laminas">
+                <a href="${element.link}" style="cursor:pointer"><img class="productImg" src="${element.items[0].images[0].imageUrl}"/></a>
+                <a href="${element.link}" style="cursor:pointer"><p class="nameLamina">${element.productName}</p></a>
+                <a href="${element.link}" style="cursor:pointer"><p class="actualPrice">$ ${element.items[0].sellers[0].commertialOffer.Price} MXN </p></a>
+                <a href="${element.items[0].sellers[0].addToCartLink}" class="productButton" style="cursor:pointer">AÑADIR A LA BOLSA</a>
+            </div>`;
+    }
+    html += "</div>";
+    container.innerHTML = html;
+    cantProd.innerHTML = arrProd.length;
+}
+
+/* funcion de llamada a la API de VTEX para obtener los productos segun un Ordenamiento*/
 function productCategoryASC(category) {
     var data;
 
@@ -24,11 +62,11 @@ function productCategoryASC(category) {
         })
         .done(function (responseData) {
             data = responseData;
-            console.log(data);
             lamProductIdCategory(data, category);
         });
 }
 
+/* funcion de llamada a la API de VTEX para obtener los productos segun un Ordenamiento*/
 function productCategoryDESC(category) {
     var data;
 
@@ -37,11 +75,11 @@ function productCategoryDESC(category) {
         })
         .done(function (responseData) {
             data = responseData;
-            console.log(data);
             lamProductIdCategory(data, category);
         });
 }
 
+/* funcion de llamada a la API de VTEX para obtener los productos segun un Ordenamiento*/
 function productCategoryBestRate(category) {
     var data;
 
@@ -50,11 +88,11 @@ function productCategoryBestRate(category) {
         })
         .done(function (responseData) {
             data = responseData;
-            console.log(data);
             lamProductIdCategory(data, category);
         });
 }
 
+/* funcion de llamada a la API de VTEX para obtener los productos segun un Ordenamiento*/
 function productCategoryReleaseDate(category) {
     var data;
 
@@ -63,11 +101,24 @@ function productCategoryReleaseDate(category) {
         })
         .done(function (responseData) {
             data = responseData;
-            console.log(data);
             lamProductIdCategory(data, category);
         });
 }
 
+/* funcion de llamada a la API de VTEX para obtener los productos segun un Ordenamiento*/
+function productCategoryBestSeller(category) {
+    var data;
+
+    $.ajax({
+            url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByTopSaleDESC&fq=C:/23/"
+        })
+        .done(function (responseData) {
+            data = responseData;
+            lamProductIdCategoryBestSeller(data, category);
+        });
+}
+
+/* se llama a esta funcion al acceder a la pagina Categorias para obtener el ID de categoria que paso por un query url*/
 function getParameterByName(catId) {
     catId = catId.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + catId + "=([^&#]*)"),
@@ -75,17 +126,27 @@ function getParameterByName(catId) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+/* se llama a esta funcion en el onchange() del select del SORT BY*/
 function myOrderOfProducts(catId) {
     let typeOfOrder = document.getElementById("selectOrder").value;
-    console.log(catId);
 
-    if (typeOfOrder == 'asc') {
-        productCategoryASC(catId);
-    } else if (typeOfOrder == 'desc') {
-        productCategoryDESC(catId);
-    } else if (typeOfOrder == 'bestrate') {
-        productCategoryBestRate(catId);
-    } else if (typeOfOrder == 'releasedate') {
-        productCategoryReleaseDate(catId);
+    switch (typeOfOrder) {
+        case "asc":
+            productCategoryASC(catId);
+            break;
+        case "desc":
+            productCategoryDESC(catId);
+            break;
+        case "bestrate":
+            productCategoryBestRate(catId);
+            break;
+        case "releasedate":
+            productCategoryReleaseDate(catId);
+            break;
+        case "bestseller":
+            productCategoryBestSeller(catId);
+            break;
+        default:
+            productCategoryASC(catId);
     }
 }
