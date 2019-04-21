7575e36e792791e7e2a7cf41d539287bcb05b5d9
Aymara <aysanchez@tesselar.mx>
2019-04-21 17:34:13 -0500

    ya funcionan los filtros, tanto por categorias como por mas vendidos y ultimos añadidos

--- js/filtrosProductos.js
+++ js/filtrosProductos.js
@@ -7,7 +7,7 @@ function filtrosDeProductos(catId) {
             })
             .done(function (responseData) {
                 data = responseData;
-                filtrosSubCategorias(data);
+                filtrosSubCategorias(data, catId);
             });
     } else {
         $.ajax({
@@ -35,17 +35,17 @@ function filtrosSubCategorias(data, catId) {
 
     let arrCategoriasUnique = getUnique(arrCategorias, 'categoriaId');
     let html = "<h2 class='filtroh2'>Categorías</h2>";
-    
+
     if (arrCategoriasUnique.length == 1) {
         html +=
             ` <label class="container">${arrCategoriasUnique[0].categoriaNombre[2]}
-                 <input type="radio" name="categoria" checked='checked' value="${arrCategoriasUnique[0].categoryId}"/>
+                 <input type="radio" name="categoria" checked='checked' value="${arrCategoriasUnique[0].categoriaId}"/>
                  <span class="checkmark"></span>
               </label>
             `;
 
     } else if (arrCategoriasUnique.length > 1) {
-        html += "<label class='container'>SIN FILTROS<input type='radio' checked='checked' name='categoria' /><span class='checkmark'></span></label>";
+        html += "<label class='container'>SIN FILTROS<input type='radio' checked='checked' name='categoria' value='" + catId + "'  /><span class='checkmark'></span></label>";
         for (var i = 0; i < arrCategoriasUnique.length; i++) {
             const element = arrCategoriasUnique[i];
             if (element.categoriaNombre.length > 4 && element.categoriaNombre[3] != "" && catId == 25) {
@@ -75,7 +75,26 @@ function filtrosSubCategorias(data, catId) {
         }
     }
     container.innerHTML = html;
-    console.log(container);
+
+    $("input[name='categoria']").on("change", function () {
+        let catIdRadio = this.value;
+        let catIdUrl = catId;
+        let especificacion = document.querySelector('input[name="especificacion"]:checked').value;
+        console.log(catIdRadio);
+        console.log(catIdUrl);
+        console.log(especificacion);
+        ajaxCallEspecification(especificacion, catIdRadio, catIdUrl);
+    });
+
+    $("input[name='especificacion']").on("change", function () {
+        let especificacion = this.value;
+        let catIdUrl = catId;
+        let catIdRadio = document.querySelector('input[name="categoria"]:checked').value;
+        console.log(catIdRadio);
+        console.log(catIdUrl);
+        console.log(especificacion);
+        ajaxCallEspecification(especificacion, catIdRadio, catIdUrl);
+    });
 }
 
 function getUnique(arr, comp) {
@@ -86,4 +105,143 @@ function getUnique(arr, comp) {
         // eliminate the dead keys & store unique objects
         .filter(e => arr[e]).map(e => arr[e]);
     return unique;
+}
+
+function lamProductByFilter(data) {
+    console.log(data);
+    console.log(data.length);
+    let container = document.getElementById('productos-categoria');
+    let cantProd = document.getElementById('cantProd');
+    let count = 0;
+    let html = "<div>";
+    if (data.length > 0) {
+        for (var i = 0; i < data.length; i++) {
+            const element = data[i];
+            count++;
+            html +=
+                `<div class="laminas">
+                <a href="${element.link}" style="cursor:pointer"><img class="productImg" src="${element.items[0].images[0].imageUrl}"/></a>
+                <a href="${element.link}" style="cursor:pointer"><p class="nameLamina">${element.productName}</p></a>
+                <a href="${element.link}" style="cursor:pointer"><p class="actualPrice">$ ${element.items[0].sellers[0].commertialOffer.Price} MXN </p></a>
+                <a href="${element.items[0].sellers[0].addToCartLink}" class="productButton" style="cursor:pointer">AÑADIR A LA BOLSA</a>
+            </div>`;
+        }
+        html += "</div>";
+        container.innerHTML = html;
+        cantProd.innerHTML = count;
+        paginadoProductos(2);
+    } else {
+        html +=
+            `<h1>No existen productos para mostrar con los filtros seleccionados</h1>`;
+        html += "</div>";
+        container.innerHTML = html;
+        cantProd.innerHTML = '0';
+    }
+}
+
+function ajaxCallEspecification(especificacion, catIdRadio, catIdUrl) {
+    let espAjax;
+    if (especificacion == 0) {
+        if (catIdUrl == 23 && catIdRadio == catIdUrl) {
+            $.ajax({
+                    url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/"
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdRadio == 25 && (catIdUrl == 23 || catIdUrl == 25)) {
+            $.ajax({
+                    url: "https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/25/"
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdUrl != 23 && catIdRadio != 25 && catIdRadio != catIdUrl) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/" + catIdUrl + "/" + catIdRadio)
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdUrl != 23 && catIdRadio != 25 && catIdRadio == catIdUrl) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/" + catIdUrl)
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdUrl == 23 && catIdRadio != catIdUrl) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/" + catIdRadio)
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        }
+    } else {
+        if (especificacion == 1) {
+            espAjax = 78;
+        } else if (especificacion == 2) {
+            espAjax = 79;
+        }
+
+        if (catIdUrl == 23 && catIdRadio == catIdUrl) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/&fq=specificationFilter_" + espAjax + ":1")
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdRadio == 25 && (catIdUrl == 23 || catIdUrl == 25)) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/25/&fq=specificationFilter_" + espAjax + ":1")
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdUrl != 23 && catIdRadio != 25 && catIdRadio != catIdUrl) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/" + catIdUrl + "/" + catIdRadio + "/&fq=specificationFilter_" + espAjax + ":1")
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdUrl != 23 && catIdRadio != 25 && catIdRadio == catIdUrl) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/" + catIdUrl + "/&fq=specificationFilter_" + espAjax + ":1")
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        } else if (catIdUrl == 23 && catIdRadio != catIdUrl) {
+            $.ajax({
+                    url: ("https://demoagro.myvtex.com/api/catalog_system/pub/products/search?_from=0&_to=49&O=OrderByNameASC&fq=C:/23/" + catIdRadio + "&fq=specificationFilter_" + espAjax + ":1")
+                })
+                .done(function (responseData) {
+                    data = responseData;
+                    console.log(data);
+                    lamProductByFilter(data);
+                });
+        }
+
+    }
+    console.log(espAjax);
 }
